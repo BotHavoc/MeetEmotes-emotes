2d289ee08ff2afa4eb6a11c8b99a91c05e8d8ce3
veehz <veehzhz@gmail.com>
2020-11-04 13:03:51 +0800

    new emotes, removed yay.png

---
+++ writer.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const filetypes = ["png","gif"]
+let data = { "filetypes": filetypes, "emotes": {"png": [], "gif": []} }
+
+fs.readdir("./emotes", (err, files) => {
+	let nft = 0;
+	filetypes.forEach(ft => {
+		let tfiles = files.filter(f => f.split(".").pop()===ft);
+		tfiles.sort();
+		let nf = 0;
+		tfiles.forEach(f => {
+			data["emotes"][ft].push(f.substring(0, f.length - ft.length - 1));
+		})
+	})
+	fs.writeFileSync('test.json', JSON.stringify(data, null, 2));
+})
+
